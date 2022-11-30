09eb77ebb7e2dfc3f2dec4bcf9a05c20b154f481
Iluha <bkmrfrbkmrf326915@mail.ru>
2022-11-30 09:36:25 +0200

    Lecture #4

---
+++ Unit4/Lecture/script.js
@@ -0,0 +1,52 @@
+// class User {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     sayHello() {
+//         console.log(this.name + "Hello");
+//     }
+// }
+
+// class Person extends User {
+//     constructor(name, email) {
+//         super(name);
+//         this.email = email;
+//     }
+//     sayHello() {
+//         super.sayHello();
+//         console.log(this.email);
+//     }
+// }
+
+// const li = new Person('li', 'li@email.ua');
+
+// // console.log(li);
+// li.sayHello();
+// li.sayHello2();
+
+
+// const alex = new User('Alex');
+// console.log(alex);
+
+// alex.sayHello();
+
+// console.log(typeof User); // function :0
+
+class User {
+    sayHello() {
+        console.log(this.name + ' Hello ');
+        return this.name;
+
+    }
+}
+
+class Person extends User {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+}
+
+const li = new Person('Li');
+console.log(li);
+li.sayHello();
