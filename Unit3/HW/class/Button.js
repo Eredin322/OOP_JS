3d4b79675404934fc625c56977819e46e93259e5
Iluha <bkmrfrbkmrf326915@mail.ru>
2022-12-01 13:03:30 +0200

    HW #3

--- Unit3/HW/class/Button.js
+++ Unit3/HW/class/Button.js
@@ -1,7 +1,21 @@
 class Button {
-
+    constructor(width, height, background, value) {
+        this.width = width;
+        this.height = height;
+        this.background = background;
+        this.value = value;
+    }
 
     render() {
-
+        const btn = document.createElement('button');
+        btn.style.width = this.width + 'px';
+        btn.style.height = this.height + 'px';
+        btn.style.background = this.background;
+        btn.innerHTML = this.value;
+        document.querySelector('body').appendChild(btn);
+        return btn;
     }
 }
+
+let btn1 = new Button(100, 100, 'blue', 'Push me');
+btn1.render();
---
+++ Unit3/HW/class/ModernButton.js
@@ -0,0 +1,13 @@
+class ModernButton extends Button {
+    constructor(width, height, background, value, borderRadius) {
+        super(width, height, background, value);
+        this.borderRadius = borderRadius;
+    }
+    render() {
+        super.render().style.borderRadius = this.borderRadius + '%';
+    }
+}
+
+let btn2 = new ModernButton(100, 100, 'blue', 'Push me', 100);
+btn2.render();
+// console.log(btn2);
