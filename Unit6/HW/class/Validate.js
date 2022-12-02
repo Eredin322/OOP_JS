007b38d1ebe464f3d2786c48a8deaed439b7f39b
Iluha <bkmrfrbkmrf326915@mail.ru>
2022-12-02 18:16:26 +0200

    HW #6

--- Unit6/HW/class/Validate.js
+++ Unit6/HW/class/Validate.js
@@ -2,4 +2,66 @@ class Validate {
     constructor(a) {
         this.a = a;
     }
+    static isNumber(number) {
+        if (typeof number === 'number') return true;
+        return false;
+    }
+    static isInt(number) {
+        if (parseInt(number) === number) return true;
+        return false;
+    }
+    static isFloat(number) {
+        if (parseInt(number) !== number) return true;
+        return false;
+    }
+    static isChar(string) {
+        if (string.length > 1) return false;
+        return true;
+    }
+    static isString(string) {
+        if (typeof string === 'string') return true;
+        return false;
+    }
+    static isBoolean(value) {
+        if (typeof value === 'boolean') return true;
+        return false;
+    }
+    static isArray(value) {
+        console.log(typeof value)
+        if (Array.isArray(value)) return true;
+        return false;
+    }
+    static toMoney(number) {
+        let res = '';
+        let arr;
+        let arr2 = [];
+        let decimal;
+        if (Validate.isString(number)) {
+            arr = number.split('').reverse();
+        } else {
+            arr = number.toString().split('').reverse();
+        }
+
+
+        if (arr.includes('.')) {
+            decimal = arr.splice(0, arr.indexOf('.') + 1)
+        }
+        console.log(decimal);
+        for (let i = 0; i < arr.length; i++) {
+            if (i !== 0 && i % 3 === 0) {
+                console.log(i);
+                arr2.push(' ');
+                arr2.push(arr[i]);
+                continue;
+            }
+            arr2.push(arr[i]);
+        }
+        res = arr2.reverse().join('');
+        if (Validate.isInt(+number) && decimal.length === 0) {
+            res = res + '.00';
+        } else {
+            res = res + decimal.reverse().join('');
+        }
+        return res;
+    }
 }
---
+++ Unit6/HW/script.js
@@ -0,0 +1,14 @@
+// let newObj = new Validate(4);
+// console.log(newObj);
+// console.log(Validate.isNumber(newObj.a)); // true;
+// console.log(Validate.isInt(3.2)); // false;
+// console.log(Validate.isFloat(3.2)); // true;
+// console.log(Validate.isChar('b')); // true;
+// console.log(Validate.isChar('bb')); // false;
+// console.log(Validate.isString('bb')); // true;
+// console.log(Validate.isString(123)); // false;
+// console.log(Validate.isBoolean(true)); // true;
+// console.log(Validate.isArray([true, 123, 'dasd'])); // true;
+// console.log(Validate.isArray('dsada')); // true;
+console.log(Validate.toMoney(200000000.54)); // true;
+
